7f0594ea51573075bf866ce1ff10c3023f049c67
Braxton Shoop <braxton.shoop@gmail.com>
2020-11-16 09:52:55 -0600

    phase 1 completed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please guess a secret number . . .");
+            string guess = Console.ReadLine();
+            Console.WriteLine(guess);
+        }
+    }
+}
